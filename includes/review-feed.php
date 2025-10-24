3f15affddedec3328f9919fd91382fd5687485c1
KaelShowTile <kaelshowtile@gmail.com>
2025-10-24 16:09:29 +1100

    2025102401
    Update review feed xml format

--- includes/review-feed.php
+++ includes/review-feed.php
@@ -18,23 +18,49 @@ function generate_product_review_feed()
     ");
 
     // Initialize XML structure
-    $xml = new SimpleXMLElement('<?xml version="1.0"?><feed></feed>');
-    $xml->addAttribute('xmlns', 'http://www.w3.org/2005/Atom');
-    $xml->addAttribute('xmlns:g', 'http://base.google.com/ns/1.0');
+    $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><feed></feed>');
+    $xml->addAttribute('xmlns:vc', 'http://www.w3.org/2007/XMLSchema-versioning');
+    $xml->addAttribute('xmlns:xsi', 'http://www.w3.org/2001/XMLSchema-instance');
+    $xml->addAttribute('xsi:noNamespaceSchemaLocation', 'http://www.google.com/shopping/reviews/schema/product/2.4/product_reviews.xsd');
+
+    // Add version
+    $xml->addChild('version', '2.4');
+
+    // Add publisher
+    $publisher = $xml->addChild('publisher');
+    $publisher->addChild('name', get_bloginfo('name'));
+    $publisher->addChild('favicon', 'https://www.cheapestiles.com.au/wp-content/uploads/2025/06/cht-fav-20250630.png'); // for cht
+
+    // Add reviews container
+    $reviews_container = $xml->addChild('reviews');
 
     foreach ($reviews as $review) {
-        $entry = $xml->addChild('entry');
-        $entry->addChild('g:review_id', $review->review_id);
-        $entry->addChild('g:product_id', htmlspecialchars($review->post_id)); 
-        $entry->addChild('g:reviewer_name', htmlspecialchars($review->customer_name));
-        $entry->addChild('g:content', htmlspecialchars($review->review_content));
-        $entry->addChild('g:review_timestamp', date('c', strtotime($review->review_date)));
-        $entry->addChild('g:rating', $review->product_rating);
+        $review_element = $reviews_container->addChild('review');
 
-        /* Add images
+        $review_element->addChild('review_id', $review->review_id);
+
+        $reviewer = $review_element->addChild('reviewer');
+        $reviewer->addChild('name', htmlspecialchars($review->customer_name));
+
+        $review_element->addChild('review_timestamp', date('c', strtotime($review->review_date)));
+        $review_element->addChild('content', htmlspecialchars($review->review_content));
+
+        $review_url = $review_element->addChild('review_url', 'https://www.cheapestiles.com.au/wp-content/plugins/glint-review/submit-review/submit-form.php#product_id=' . htmlspecialchars($review->post_id));
+        $review_url->addAttribute('type', 'singleton');
+
+        $ratings = $review_element->addChild('ratings');
+        $overall = $ratings->addChild('overall', $review->product_rating);
+        $overall->addAttribute('min', '1');
+        $overall->addAttribute('max', '5');
+
+        $products = $review_element->addChild('products');
+        $product = $products->addChild('product');
+        $product->addChild('product_url', get_permalink(htmlspecialchars($review->post_id)));
+
+        /* Add images if needed
         $images = unserialize($review->review_imgs);
         foreach ($images as $image_url) {
-            $entry->addChild('g:image_url', $image_url);
+            // Note: The sample doesn't include images, so skipping for now
         } */
     }
 
@@ -45,4 +71,3 @@ function generate_product_review_feed()
         error_log("Error saving file.");
     }
 }
-
